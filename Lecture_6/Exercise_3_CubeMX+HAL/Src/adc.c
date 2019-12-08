02269b471887781cfabffd3414688da043965ac9
Roman Kukui <roman.kukui@gmail.com>
2019-12-08 12:41:04 +0200

    Exercise-3: Move array methods to Lib. Increas ADC to 12bit. Other misc improvments.

--- Lecture_6/Exercise_3_CubeMX+HAL/Src/adc.c
+++ Lecture_6/Exercise_3_CubeMX+HAL/Src/adc.c
@@ -37,7 +37,7 @@ void MX_ADC1_Init(void)
   */
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
-  hadc1.Init.Resolution = ADC_RESOLUTION_8B;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
   hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
   hadc1.Init.ContinuousConvMode = ENABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
@@ -102,8 +102,8 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
     hdma_adc1.Init.Direction = DMA_PERIPH_TO_MEMORY;
     hdma_adc1.Init.PeriphInc = DMA_PINC_DISABLE;
     hdma_adc1.Init.MemInc = DMA_MINC_ENABLE;
-    hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-    hdma_adc1.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+    hdma_adc1.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
     hdma_adc1.Init.Mode = DMA_NORMAL;
     hdma_adc1.Init.Priority = DMA_PRIORITY_LOW;
     if (HAL_DMA_Init(&hdma_adc1) != HAL_OK)
--- Lecture_6/Exercise_3_CubeMX+HAL/Src/main.c
+++ Lecture_6/Exercise_3_CubeMX+HAL/Src/main.c
@@ -29,6 +29,8 @@
 /* USER CODE BEGIN Includes */
 	#include <string.h>
 	#include <stdio.h>
+	
+	#include <../Lib/arr.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -38,6 +40,9 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+	
+	#define ADC_SAMPLES 5
+	
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -50,8 +55,8 @@
 /* USER CODE BEGIN PV */
 
 	/// store current ADC 8-bit values
-	uint8_t adc_curr[5];
-	uint8_t adc_prev[5];
+	uint32_t adc_curr[ADC_SAMPLES];
+	uint32_t adc_prev[ADC_SAMPLES];
 	
 	/// store ADC conversion compleate flag
 	uint8_t adc_cplt_f = 0;
@@ -62,66 +67,12 @@
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
 
-/** \brief	Send/print arr* content to USART1 as integer values + "\n\r".
- *
- * \param[in] *arr	Pointer to array, that will be printed.
- * \param[in] size	Number of elements, that will be printed.
- *
- * \note	Used UART polling mode with HAL_MAX_DELAY timeout.
- */
-void arr_print(uint8_t *arr, size_t size)
-{
-	for (size_t i = 0; i < size; i++) {
-		uint8_t msg[15];
-		sprintf((char*)msg, "%i\t", arr[i]);
-		HAL_UART_Transmit(&huart1, msg, strlen((char*)msg), HAL_MAX_DELAY);
-	}
-	uint8_t ret_msg[] = {"\n\r"};
-	HAL_UART_Transmit(&huart1, ret_msg, 2, HAL_MAX_DELAY);
-}
-
-/** \brief	Compare elements of two arrays.
- *
- * \param[in] *arr1	Pointer to array #1.
- * \param[in] *arr2	Pointer to array #2.
- * \param[in] size	Number of elements for comparing (size of array).
- *
- * \note 	Will be compared only \p size number of elements.
- *
- * \retval -1		Arrays elements identical.
- * \retval 		Index of first different element.
- */
-int8_t arr_cmp(uint8_t *arr1, uint8_t *arr2, size_t size)
-{
-	int8_t ret_val = -1;		// default value, arrays identical
-	
-	for (size_t i = 0; i < size; i++) {
-		if (arr1[i] != arr2[i]) {
-			ret_val = i;
-			break;
-		}
-	}
-	return ret_val;
-}
-
-/** \brief	Copy \p size elements from \p *src to \p *dst array.
- *
- * \param[out] *dst	Pointer to destination array.
- * \param[in] *src	Pointer to source array.
- * \param[in] size	Number of bytes, that will be copied.
- */
-void arr_cpy(uint8_t *dst, uint8_t *src, size_t size)
-{
-	for (size_t i = 0; i < size; i++) {
-		dst[i] = src[i];
-	}
-}
 
 /** \brief	Set conversion compleat flag (redefined callback).
  *
  * \param[in] hadc	Pointer to ADC control struct.
  */
-void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc)
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
 	adc_cplt_f = 1;		// set conversion completed flag
 }
@@ -167,7 +118,7 @@ int main(void)
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   
-	uint8_t welcome_msg[] = {"\n\rStartup initialisation completed\n\r"};
+	uint8_t welcome_msg[] = {"\n\r12bit ADC1 ch1 - PA0 (Blue button) values:\n\r"};
 	HAL_UART_Transmit(&huart1, welcome_msg, strlen((char*)welcome_msg), 1000);
 
   /* USER CODE END 2 */
@@ -175,30 +126,25 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 	
-	// uint8_t* pointer converted to uint32_t* because used ADC 8-bit mode
-	// and 1 byte DMA packet size
-	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_curr, 5);
+	HAL_ADC_Start_DMA(&hadc1, adc_curr, ADC_SAMPLES);
 	
   while (1)
   {
 	if (adc_cplt_f == 1) {
 		// test for changing ADC measurements relative to adc_prev[]
 		int8_t res = -1;
-		res = arr_cmp(adc_prev, adc_curr, sizeof(adc_curr));
+		res = arr_cmp(adc_prev, adc_curr, ADC_SAMPLES);
 		
 		if ( res >= 0) {
 			// display measured(changed) values
-			arr_print(adc_curr, sizeof(adc_curr));
+			arr_print(adc_curr, ADC_SAMPLES);
 			
-			// 'shift' to previous value
-			arr_cpy(adc_prev, adc_curr, sizeof(adc_prev));
+			// 'shift' current -> previous
+			arr_cpy(adc_prev, adc_curr, ADC_SAMPLES);
 		}
-		
 		adc_cplt_f = 0;		// clear conversion compleated flag
 		
-		// uint8_t* pointer converted to uint32_t* because used ADC 8-bit mode
-		// and 1 byte DMA packet size
-		HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_curr, 5);
+		HAL_ADC_Start_DMA(&hadc1, adc_curr, ADC_SAMPLES);
 		
 		HAL_Delay(50);		// user friendly speed down
 	}
